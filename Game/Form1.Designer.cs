93d8c71e2b905b117a455497b2959e77491aeaa4
bjoonly <elumnaya@gmail.com>
2020-10-29 17:14:16 +0200

    Init

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.RuleLabel = new System.Windows.Forms.Label();
+            this.RulesLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RuleLabel
+            // 
+            this.RuleLabel.AutoSize = true;
+            this.RuleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RuleLabel.Location = new System.Drawing.Point(11, 28);
+            this.RuleLabel.Name = "RuleLabel";
+            this.RuleLabel.Size = new System.Drawing.Size(61, 25);
+            this.RuleLabel.TabIndex = 2;
+            this.RuleLabel.Text = "Rules";
+            // 
+            // RulesLabel
+            // 
+            this.RulesLabel.AutoSize = true;
+            this.RulesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RulesLabel.Location = new System.Drawing.Point(12, 66);
+            this.RulesLabel.Name = "RulesLabel";
+            this.RulesLabel.Size = new System.Drawing.Size(229, 40);
+            this.RulesLabel.TabIndex = 3;
+            this.RulesLabel.Text = "Guess a number from 1 to 100 \r\nand I will try to guess it\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 274);
+            this.Controls.Add(this.RulesLabel);
+            this.Controls.Add(this.RuleLabel);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label RuleLabel;
+        private System.Windows.Forms.Label RulesLabel;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numLow = 1, numUp = 101, current = rand.Next(numLow, numUp);
+            int count = 0;
+            DialogResult choose = DialogResult.Yes;
+            do
+            {
+                count++;
+                var res = MessageBox.Show($"{+current}\nIs this your number?", "Game", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (res == DialogResult.No)
+                {
+                    var choose1 = MessageBox.Show("Is your number bigger?", "Game", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (choose1 == DialogResult.Yes)
+                    {
+                        numLow = current + 1;
+                    }
+                    else
+                    {
+                        numUp = current;
+                    }
+                    current = rand.Next(numLow, numUp);
+
+                }
+
+                else if (res == DialogResult.Yes)
+                {
+                    choose = MessageBox.Show("Count: " + count + "\nPlay again?", "Game", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    count = 0;
+                    if (choose == DialogResult.No)
+                    {
+                        MessageBox.Show("Bye", "Game", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                    }
+
+                }
+                else
+                {
+                    Close();
+                }
+            } while (choose != DialogResult.No);
+        }
+    }
+}
